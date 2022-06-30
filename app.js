4698de23821fe7bd5efd54e8b1c662d4b2b55ceb
sirAbhishek35 <kumarabhishek8544240107@gmail.com>
2022-06-30 13:11:01 +0530

    change

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'ejs');
 
 const dbURI = process.env.MONGODB_URI;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(process.env.PORT || 3000))
   .catch((err) => console.log(err));
 
 
